c186168783b7afa81026927f7db97f4bc6de9358
Xavier Valarino <xavier@Xaviers-MBP.lan>
2020-05-19 22:15:16 -0700

    Make EFI update script run smoother

--- scripts/update-efi.sh
+++ scripts/update-efi.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 sudo mkdir /Volumes/EFI
 sudo diskutil mount -mountPoint /Volumes/EFI /dev/disk0s1
-rsync -au --progress $HOME/hackintosh/XPS13-9360/OC/ /Volumes/EFI/EFI/OC/
+rsync -ac --stats $HOME/hackintosh/XPS13-9360/OC/ /Volumes/EFI/EFI/OC/
+# Give finder a chance to disengage
+sleep 1;  
 diskutil umount /dev/disk0s1
